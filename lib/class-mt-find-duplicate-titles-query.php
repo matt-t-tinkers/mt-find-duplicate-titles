20542eba0e588296363d09173daeefd15910e5d9
Matt Taylor <2266932+matt-t-tinkers@users.noreply.github.com>
2021-12-16 14:57:07 +0000

    assembled the db query and tested it out

--- lib/class-mt-find-duplicate-titles-query.php
+++ lib/class-mt-find-duplicate-titles-query.php
@@ -1,1 +1,37 @@
 <?php
+/**
+ * Build a query to find duplicate pages that have been published and output the results
+ */
+
+if ( ! class_exists( 'FindDuplicateTitlesQuery' ) ) {
+
+	class FindDuplicateTitlesQuery {
+
+		public function __construct() {
+			/**
+			 * Adds an action hook to run the query
+			 */
+			add_action( 'dupesPageQuery', array( $this, 'get_duplicate_titles' ) );
+		}
+
+        /**
+         * Using wpdb to query the database, find the title of posts where the post is published and the title appears more than once
+         */
+        public function get_duplicate_titles() {
+            global $wpdb;
+
+            // Get a list of postmeta tables.
+            $post_dupes = $wpdb->get_results( "SELECT post_title FROM wp_posts WHERE post_status = 'publish' GROUP BY post_title HAVING count(*) >1");
+            $titles = array();
+            foreach($post_dupes as $post) {
+                $titles[] = $post->post_title;
+            }
+            // Output the array.
+            print_r ($titles);
+        }
+
+    }
+
+    new FindDuplicateTitlesQuery();
+}
+
--- lib/class-mt-find-duplicate-titles.php
+++ lib/class-mt-find-duplicate-titles.php
@@ -5,25 +5,22 @@
 
 if ( ! class_exists( 'FindDuplicateTitles' ) ) {
 	/**
-	 * Class to provide tools to set up pages in the network admin area
-	 * of a Multisite minstall and load data via ajax.
+	 * Class to add a tool to find duplicate post titles in a site
 	 */
 	class FindDuplicateTitles {
-		/**
-		 * Constructor - add network settings page to accomodate tools
-		 */
+
 		public function __construct() {
 			/**
-			 * Adds the network settings page for the tools
+			 * Set up a page in the dashboard
 			 */
 			add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
 		}
 
 		/**
-		 * Adds pages to the Network admin area and loads any supporting scripts.
+		 * Add page to the Tools menu
 		 */
 		public function add_admin_pages() {
-            add_management_page( 'Find duplicate titles', 'Find duplicate titles', 'install_plugins', 'find-dupe-titles', array( $this, 'display_dupes_page' ), 10 );
+            add_management_page( 'Find duplicate titles', 'Find Duplicate Titles', 'install_plugins', 'find-dupe-titles', array( $this, 'display_dupes_page' ), 10 );
 		}
 
 		/**
@@ -31,7 +28,10 @@ if ( ! class_exists( 'FindDuplicateTitles' ) ) {
 		 */
 		public function display_dupes_page() {
 			print( '<div class="wrap"><h2>Find Duplicated Post Titles</h2>' );
+            print( '<pre>' );
             // Output query result.
+            do_action('dupesPageQuery');
+            print( '</pre>' );
 			print( '</div>' );
 		}
 
